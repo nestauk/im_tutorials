07b2d8f2d8ce1351d444ed02576baeb3c7b1dc78
George Richardson <georgerichardson@posteo.net>
2019-03-11 17:12:24 +0000

    change readme

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name='im_tutorials',
     packages=find_packages(),
     version='0.1.0',
     description='Tutorial materials from Innovation Mapping team workshops.',
