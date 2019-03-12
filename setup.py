de5ddc8df6b43c681579312802b86bf4c1c3c9cb
George Richardson <georgerichardson@posteo.net>
2019-03-12 11:10:45 +0000

    add cooccurrence network explaination

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='im_tutorials',
+    name='im-tutorials',
     packages=find_packages(),
     version='0.1.0',
     description='Tutorial materials from Innovation Mapping team workshops.',
