aedd36b8778f4277d37ae4d4e6b6b9292fe2094c
George Richardson <georgerichardson@posteo.net>
2019-03-20 15:48:51 +0000

    add flatten utility

--- im_tutorials/utilities.py
+++ im_tutorials/utilities.py
@@ -1,3 +1,4 @@
+import itertools
 import numpy as np
 
 def chunks(l, n):
@@ -18,3 +19,9 @@ def print_counter_most_common(counter, n, name='Item', padding=30, stats=True):
         print(f'\nLower Quartile: {lo}')
         print(f'Median:           {med}')
         print(f'Upper Quartile:   {up}')
+
+def flatten_lists(l):
+    '''flatten_lists
+    Unpacks nested lists into one list of elements.
+    '''
+    return list(itertools.chain(*l))
