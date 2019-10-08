6f66e4dbaa5f7a2d71214c0575be6c5e74a9ce6f
George Richardson <georgerichardson@posteo.net>
2019-10-08 15:40:10 +0200

    add function for sdg web data articles

--- im_tutorials/data/datasets.py
+++ im_tutorials/data/datasets.py
@@ -30,6 +30,20 @@ def gateway_to_research_projects():
     )
     return gtr_projects_df
 
+def sdg_web_articles():
+    '''sdg_web_articles
+    Get SDG web articles scraped from web and return as DataFrame.
+    '''
+    bucket='innovation-mapping-tutorials'
+    gtr_projects_key='sdg/sdg_web_articles.json'
+    list_cols = ['sdg_goals']
+    gtr_projects_df = pd.read_json(
+        smart_open.smart_open("https://s3.us-east-2.amazonaws.com/{}/{}".format(bucket, gtr_projects_key)),
+        converters=eval_cols(list_cols),
+        index_col=0
+    )
+    return gtr_projects_df
+
 def double_eval(x):
     return ast.literal_eval(ast.literal_eval(x))
 
